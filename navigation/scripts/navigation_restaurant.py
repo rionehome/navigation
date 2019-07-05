4e7241dcd6dbdb57da31ab452fd7988a81aacb9c
Nenetti <akinori1090@gmail.com>
2019-07-06 04:21:19 +1000

    [fix] 距離を修正

--- navigation/scripts/navigation_restaurant.py
+++ navigation/scripts/navigation_restaurant.py
@@ -48,7 +48,7 @@ class NavigationHumanDetect:
         if rotation.z < 0:
             angle *= -1
 
-        x = translation.x + range * math.cos(angle) - 1.0
+        x = translation.x + range * math.cos(angle) - 0.5
         y = translation.y + range * math.sin(angle)
 
         pose = Pose()
