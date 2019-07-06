11bbc322f1a8ffa2cf74f122694bc06a4226afe2
Nenetti <akinori1090@gmail.com>
2019-07-06 12:12:38 +1000

    [fix] 向き修正

--- navigation/scripts/navigation_restaurant.py
+++ navigation/scripts/navigation_restaurant.py
@@ -49,7 +49,7 @@ class NavigationHumanDetect:
         if rotation.z < 0:
             angle *= -1
 
-        x = translation.x + range * math.cos(angle) - 0.7
+        x = translation.x + range * math.cos(angle) - 0.5
         y = translation.y + range * math.sin(angle)
 
         pose = Pose()
